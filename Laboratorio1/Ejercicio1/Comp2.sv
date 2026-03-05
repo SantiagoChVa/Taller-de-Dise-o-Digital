9a5153163c71076f3ccfa972651e7ec39b5a3afe
SantiagoChVa <sachavarria@estudiantec.cr>
2026-03-05 11:40:50 -0600

    Create Comp2 module for 4-bit inversion
    Implemented a 4-bit inverter with a 1 added to the output.

--- Laboratorio1/Ejercicio1/Comp2.sv
+++ Laboratorio1/Ejercicio1/Comp2.sv
@@ -1,1 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05.03.2026 10:48:03
+// Design Name: 
+// Module Name: Comp2
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
 
+module comp2 (
+    input  [3:0] SW,
+    output [3:0] LED
+);
+
+assign LED = ~SW + 1;
+
+endmodule
