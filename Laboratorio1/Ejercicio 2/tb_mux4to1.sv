729266842d03ad109c92e21d5fd6f7a2b02900e2
emanuelvarelap <emanuelvarelap@gmail.com>
2026-03-20 20:57:22 -0600

    Update tb_mux4to1.sv
    Se utilizó $srandom para inicializar la semilla del generador pseudoaleatorio y $urandom para crear los datos de prueba, ya que en SystemVerilog es una opción más adecuada que $random para testbenches.

--- Laboratorio1/Ejercicio 2/tb_mux4to1.sv
+++ Laboratorio1/Ejercicio 2/tb_mux4to1.sv
@@ -17,7 +17,6 @@ module tb_mux4to1;
     logic [1:0] sel_16;
     logic [15:0] y_16;
 
-
     // Instancia MUX 4 bits
     mux4to1 #(.WIDTH(4)) mux4 (
         .in0(in0_4),
@@ -48,86 +47,76 @@ module tb_mux4to1;
         .y(y_16)
     );
 
-
     initial begin
+        $srandom(1234);  
 
         // TEST WIDTH = 4
-    
         repeat(50) begin
+            in0_4 = $urandom;
+            in1_4 = $urandom;
+            in2_4 = $urandom;
+            in3_4 = $urandom;
 
-            in0_4 = $random;
-            in1_4 = $random;
-            in2_4 = $random;
-            in3_4 = $random;
-
-            for(int i=0;i<4;i++) begin
+            for (int i = 0; i < 4; i++) begin
                 sel_4 = i;
                 #1;
 
-                case(sel_4)
-                    2'b00: if(y_4!=in0_4) $fatal(1,"Error WIDTH4 sel00");
-                    2'b01: if(y_4!=in1_4) $fatal(1,"Error WIDTH4 sel01");
-                    2'b10: if(y_4!=in2_4) $fatal(1,"Error WIDTH4 sel10");
-                    2'b11: if(y_4!=in3_4) $fatal(1,"Error WIDTH4 sel11");
+                case (sel_4)
+                    2'b00: if (y_4 != in0_4) $fatal(1, "Error WIDTH4 sel00");
+                    2'b01: if (y_4 != in1_4) $fatal(1, "Error WIDTH4 sel01");
+                    2'b10: if (y_4 != in2_4) $fatal(1, "Error WIDTH4 sel10");
+                    2'b11: if (y_4 != in3_4) $fatal(1, "Error WIDTH4 sel11");
                 endcase
             end
         end
 
         $display("WIDTH 4 OK");
 
-
         // TEST WIDTH = 8
-
         repeat(50) begin
+            in0_8 = $urandom;
+            in1_8 = $urandom;
+            in2_8 = $urandom;
+            in3_8 = $urandom;
 
-            in0_8 = $random;
-            in1_8 = $random;
-            in2_8 = $random;
-            in3_8 = $random;
-
-            for(int i=0;i<4;i++) begin
+            for (int i = 0; i < 4; i++) begin
                 sel_8 = i;
                 #1;
 
-                case(sel_8)
-                    2'b00: if(y_8!=in0_8) $fatal(1,"Error WIDTH8 sel00");
-                    2'b01: if(y_8!=in1_8) $fatal(1,"Error WIDTH8 sel01");
-                    2'b10: if(y_8!=in2_8) $fatal(1,"Error WIDTH8 sel10");
-                    2'b11: if(y_8!=in3_8) $fatal(1,"Error WIDTH8 sel11");
+                case (sel_8)
+                    2'b00: if (y_8 != in0_8) $fatal(1, "Error WIDTH8 sel00");
+                    2'b01: if (y_8 != in1_8) $fatal(1, "Error WIDTH8 sel01");
+                    2'b10: if (y_8 != in2_8) $fatal(1, "Error WIDTH8 sel10");
+                    2'b11: if (y_8 != in3_8) $fatal(1, "Error WIDTH8 sel11");
                 endcase
             end
         end
 
         $display("WIDTH 8 OK");
 
-
         // TEST WIDTH = 16
-
         repeat(50) begin
+            in0_16 = $urandom;
+            in1_16 = $urandom;
+            in2_16 = $urandom;
+            in3_16 = $urandom;
 
-            in0_16 = $random;
-            in1_16 = $random;
-            in2_16 = $random;
-            in3_16 = $random;
-
-            for(int i=0;i<4;i++) begin
+            for (int i = 0; i < 4; i++) begin
                 sel_16 = i;
                 #1;
 
-                case(sel_16)
-                    2'b00: if(y_16!=in0_16) $fatal(1,"Error WIDTH16 sel00");
-                    2'b01: if(y_16!=in1_16) $fatal(1,"Error WIDTH16 sel01");
-                    2'b10: if(y_16!=in2_16) $fatal(1,"Error WIDTH16 sel10");
-                    2'b11: if(y_16!=in3_16) $fatal(1,"Error WIDTH16 sel11");
+                case (sel_16)
+                    2'b00: if (y_16 != in0_16) $fatal(1, "Error WIDTH16 sel00");
+                    2'b01: if (y_16 != in1_16) $fatal(1, "Error WIDTH16 sel01");
+                    2'b10: if (y_16 != in2_16) $fatal(1, "Error WIDTH16 sel10");
+                    2'b11: if (y_16 != in3_16) $fatal(1, "Error WIDTH16 sel11");
                 endcase
             end
         end
 
         $display("WIDTH 16 OK");
-
         $display("TODAS LAS PRUEBAS PASARON");
         $finish;
-
     end
 
 endmodule
